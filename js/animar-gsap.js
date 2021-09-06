ec4ec2fe26b13629de442fa2399d5a2804c6f944
edsonrosales6 <edsonrosaleslugo@outlook.com>
2021-09-06 00:51:53 -0500

    se agregan animaciones a los titulos de las secciones

--- js/animar-gsap.js
+++ js/animar-gsap.js
@@ -1,21 +1,29 @@
-gsap.from(".proyectos", {
-    scrollTrigger: {
-        trigger: ".proyectos",
-        toggleActions: "restart pause reverse none"
-    },
-    x: 100,
-    opacity: 0,
-    ease: 'back', 
-    duration: 0.6,
-    delay: 1
-});
-
+gsap.registerPlugin(ScrollTrigger)
 gsap.from('#hola', {
     duration: 1.5,
     x: -100,
     opacity:0,
     ease: 'bounce',
-    delay: 1
+    delay: .5
 });
+
+
+const titles = gsap.utils.toArray('.slideTextLeft')
+
+titles.forEach((text, i) => {
+
+    gsap.from(text, {
+        scrollTrigger: {
+            trigger: text,
+            toggleActions: "restart pause restart"
+        },
+        x: -100,
+        opacity: 0,
+        ease: 'back', 
+        duration: 0.6,
+        delay: .5
+    });
+});
+
 
 
