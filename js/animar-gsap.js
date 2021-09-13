59830d9de23336e68db5de5de9e43441115c7c0e
edsonrosales6 <edsonrosaleslugo@outlook.com>
2021-09-13 17:53:45 -0500

    Se limpia código y se elimina código sin uso

--- js/animar-gsap.js
+++ js/animar-gsap.js
@@ -1,10 +1,9 @@
 gsap.registerPlugin(ScrollTrigger)
 gsap.from('#hola', {
-    duration: 1.5,
+    duration: .8,
     x: -100,
     opacity:0,
-    ease: 'bounce',
-    delay: .5
+    ease: 'slow'
 });
 
 
@@ -15,13 +14,11 @@ titles.forEach((text, i) => {
     gsap.from(text, {
         scrollTrigger: {
             trigger: text,
-            toggleActions: "restart pause restart"
         },
         x: -100,
         opacity: 0,
-        ease: 'back', 
-        duration: 0.6,
-        delay: 0.5
+        ease: 'slow', 
+        duration: .3
     });
 });
 
--- js/animar-gsap2.js
+++
@@ -1,8 +0,0 @@
-gsap.from('.image-animation', {
-    duration: 0.3,
-    y: -200,
-    opacity: 0,
-    delay: 0.5,
-    stagger: 0.5,
-    ease: 'back'
-});
