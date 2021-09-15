bffd0e0bf56af222052f88bfef0c7a532d6b00ec
edsonrosales6 <edsonrosaleslugo@outlook.com>
2021-09-14 20:43:19 -0500

    Se implementa formulario de contacto funcional.

--- js/contacto.js
+++ js/contacto.js
@@ -1,31 +1,11 @@
 function planBasico() {
-    document.contactForm.asunto.value = "¡Me interesa el plan básico!";
+    $('#asunto').val('¡Me interesa el plan básico!');
 }
 
 function planPro() {
-    document.contactForm.asunto.value = "¡Me interesa el plan professional!";
+    $('#asunto').val('¡Me interesa el plan profesional!');
 }
 
 function planPremium() {
-    document.contactForm.asunto.value = "¡Me interesa el plan premium!";
+    $('#asunto').val('¡Me interesa el plan premium!');
 }
-
-$('#contactForm').submit(function(e){
-    e.preventDefault()
-
-
-    Swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: 'Gracias por contactarme!',
-        showConfirmButton: false,
-        timer: 1500,
-        timerProgressBar: true,
-        iconColor: '#ff9800',
-    });
-    const form = new FormData(this)
-    const $buttonMailto = document.querySelector('#btnMailto')
-    $buttonMailto.setAttribute('href', `mailto:edsonrosales123@gmail.com?subject=${form.get('asunto')}&body=${form.get('mensaje')}`)
-    $buttonMailto.click()
-
-});
